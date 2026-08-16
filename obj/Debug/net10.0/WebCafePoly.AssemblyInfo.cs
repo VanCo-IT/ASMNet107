db2fe0bc3c364be8fe4e757ed2f00fbdd5ab593e
vanCo-IT <colv51614@gmail.com>
2026-08-16 17:45:12 +0700

    Change CoreUI delete from table NhanVien

--- obj/Debug/net10.0/WebCafePoly.AssemblyInfo.cs
+++ obj/Debug/net10.0/WebCafePoly.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebCafePoly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d365bda634d018a2d580441a66836c93154a1cbe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86ad6216b1e22d0659a01acccb91f1beca923baf")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebCafePoly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebCafePoly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
