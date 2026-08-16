e2007a7ad049958b334c5c7c147da19fa1b948a1
vanCo-IT <colv51614@gmail.com>
2026-08-16 13:28:18 +0700

    change table ChiTietPhieu - UI admin new

--- obj/Debug/net10.0/WebCafePoly.AssemblyInfo.cs
+++ obj/Debug/net10.0/WebCafePoly.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebCafePoly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69207596cf59c0f67cb6c882d8ba689734799eb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df5aa22d2a7e7fae49e26d5d2cfa2fdcfd74d1bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebCafePoly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebCafePoly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
