43e8b5cb288fbf59f3b143e34ba537233f896bef
vanCo-IT <colv51614@gmail.com>
2026-08-16 17:33:33 +0700

    Change CoreUI create  from table NhanVien

--- obj/Debug/net10.0/WebCafePoly.AssemblyInfo.cs
+++ obj/Debug/net10.0/WebCafePoly.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebCafePoly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eef8f15e85bf3299361fee7a3f6a6f520737c4f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ada057b5d9aefa8c5325d470c815e0c70e9f7f0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebCafePoly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebCafePoly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
