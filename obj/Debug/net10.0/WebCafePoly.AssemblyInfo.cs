78176277eed52da31584335e91862fad02e8f8c3
vanCo-IT <colv51614@gmail.com>
2026-08-16 14:59:21 +0700

    Update UI details,from table SanPham

--- obj/Debug/net10.0/WebCafePoly.AssemblyInfo.cs
+++ obj/Debug/net10.0/WebCafePoly.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebCafePoly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b6ddb7c265f9646d6059ee16b24c1c1db6993da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc63f953a18888113dcb7b157cd202a3eb3a3698")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebCafePoly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebCafePoly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
